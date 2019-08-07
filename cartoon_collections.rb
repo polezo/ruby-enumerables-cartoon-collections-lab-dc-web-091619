c7d8e848c9219458bf93bcfe01d705d1fce0c74e
Adam Finch <atyreefinch@gmail.com>
2019-08-07 15:25:56 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -3,7 +3,7 @@ def roll_call_dwarves (arr)
 end
 
 def summon_captain_planet (arr)
-  arr.collect {|element| eleme
+  arr.collect {|elemental| "#{elemental.capitalize}!"}
 end
 
 def long_planeteer_calls# code an argument here
