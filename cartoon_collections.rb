0fbacfc70f11f2504f82da952d98a04af8e41231
Adam Finch <atyreefinch@gmail.com>
2019-08-07 15:29:06 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -6,8 +6,8 @@ def summon_captain_planet (arr)
   arr.collect {|elemental| "#{elemental.capitalize}!"}
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls (arr)
+  arr.reduce {|elemental| if elemental.length > 4 return true}
 end
 
 def find_the_cheese# code an argument here
